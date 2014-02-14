07d3871a48739c52ec83d8c2c44f1eba335682d7
nbalaguer <karoshi.aimai@gmail.com>
2014-02-14 16:24:18 +0100

    Creadas Interfaz Etapa y etapas concretas

--- src/jpipe/CPU.java
+++ src/jpipe/CPU.java
@@ -1,5 +1,35 @@
 package jpipe;
 
-public class CPU {
+import java.util.ArrayList;
 
+public class CPU {
+	
+	private ArrayList<Etapa> etapas;
+
+	public CPU() {
+		
+	}
+	
+	public void inicializa() {
+		
+	}
+	
+	public void empieza() {
+	
+		try {
+			while ( true ) {
+				for ( Etapa etapa : etapas ) {
+					etapa.lee();
+					etapa.ejecuta();
+					etapa.escribe();
+				}
+			}
+		} catch ( ExcepcionTrap e ) {
+			this.finalizar();
+		}
+	}
+	
+	public void finalizar() {
+		
+	}
 }
