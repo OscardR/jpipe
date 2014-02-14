8bb1e6bec26bd8e439d1b29830832a3ac4abee22
lutrero <luis.trevino@uji.es>
2014-02-14 16:14:12 +0100

    Añadido ifid, idex, memeodatosd

---
+++ src/jpipe/ID_EX.java
@@ -0,0 +1,14 @@
+package jpipe;
+
+public class ID_EX {
+	
+	int a, b, ra , rb, rc, cp, op;
+
+	public ID_EX() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ src/jpipe/IF_ID.java
@@ -0,0 +1,43 @@
+package jpipe;
+
+public class IF_ID {
+	
+	int cp;
+	Instruccion ins;
+	
+	
+	public IF_ID() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public IF_ID(int cp, Instruccion ins) {
+		super();
+		this.cp = cp;
+		this.ins = ins;
+	}
+
+
+	public int getCp() {
+		return cp;
+	}
+
+
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+
+
+	public Instruccion getIns() {
+		return ins;
+	}
+
+
+	public void setIns(Instruccion ins) {
+		this.ins = ins;
+	}
+	
+	
+
+}
